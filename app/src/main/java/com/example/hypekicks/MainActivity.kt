da4ed7e65b635e65f1b3c0993df2b1d2ad55bf23
ignacy11 <ignacy.banet@cyfroweszkoly.pl>
2026-04-26 01:48:00 +0200

    fixed GridView not displaying data

--- app/src/main/java/com/example/hypekicks/MainActivity.kt
+++ app/src/main/java/com/example/hypekicks/MainActivity.kt
@@ -9,15 +9,14 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.hypekicks.databinding.ActivityMainBinding
 import com.google.firebase.Firebase
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
-import kotlin.text.clear
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    lateinit var sneakersList: MutableList<Sneakers>
-    lateinit var adapter: SneakerAdapter
-    private lateinit var allSneakersList: MutableList<Sneakers>
+    lateinit var sneakersList: MutableList<Sneaker>
+    lateinit var sneakerAdapter: SneakerAdapter
+    private lateinit var allSneakersList: MutableList<Sneaker>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,20 +29,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         sneakersList = mutableListOf()
+        allSneakersList = mutableListOf()
 
-        adapter = SneakerAdapter(this, sneakersList)
-        val db = FirebaseFirestore.getInstance()
+        sneakerAdapter = SneakerAdapter(this, sneakersList)
+        binding.sneakersGridView.adapter = sneakerAdapter
 
         fetchDataFromCloud()
         binding.sneakersGridView.setOnItemClickListener {_, _, position, _ ->
-        val clickedSneaker = sneakersList[position]
-        Toast.makeText(this,"this will do something soon", Toast.LENGTH_SHORT).show()
+            val clickedSneaker = sneakersList[position]
+            Toast.makeText(this,"this will do something soon", Toast.LENGTH_SHORT).show()
         }
 
 
-        binding.sneakersSearchview.setOnQueryTextListener(object: androidx.appcompat.widget.SearchView.OnQueryTextListener {
+        binding.sneakersSearchView.setOnQueryTextListener(object: androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                return false
+                return true
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
@@ -53,26 +53,32 @@ class MainActivity : AppCompatActivity() {
 
         })
     }
+
     private fun fetchDataFromCloud() {
-        val db = FirebaseFirestore.getInstance()
+        val db = Firebase.firestore
         db.collection("sneakers")
             .get()
             .addOnSuccessListener { documents ->
-                sneakersList.clear()
+                allSneakersList.clear()
 
                 for (document in documents) {
                     val brand = document.getString("brand")?: "Nieznana marka"
-                    val imageUrl = document.getString("imageUrl")?: ""
+                    val imageUrl = document.getString("imageUrl")?: "Nie znaleziono url zdjęcia"
                     val modelName = document.getString("modelName")?: "Nieznany model"
                     val releaseYear = document.getLong("releaseYear")?.toInt() ?: 0
                     val resellPrice = document.getLong("resellPrice")?.toInt() ?: 0
 
-                    val sneaker = Sneakers(brand, imageUrl, modelName, releaseYear, resellPrice)
+                    val sneaker = Sneaker(brand, imageUrl, modelName, releaseYear, resellPrice)
                     allSneakersList.add(sneaker)
                 }
                 sneakersList.clear()
                 sneakersList.addAll(allSneakersList)
-                adapter.notifyDataSetChanged()
+                Log.d("sneakers-output","$sneakersList")
+                try {
+                    sneakerAdapter.notifyDataSetChanged()
+                } catch (err: Error) {
+                    Log.e("set-data-err",err.toString())
+                }
             }
             .addOnFailureListener { exception ->
                 Log.e("FIREBASE_ERROR", "Błąd pobierania danych: ", exception)
@@ -94,69 +100,68 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        adapter.notifyDataSetChanged()
+        sneakerAdapter.notifyDataSetChanged()
     }
 }
-private fun seeDataBase() {
+/*
+private fun seedDatabase() {
     val sneakersList = listOf(
-        Sneakers(
+        Sneaker(
             brand = "Nike",
             imageUrl = "https://i.postimg.cc/cCqcwPTk/nike-air-max.jpg",
             modelName = "Air Max 90",
             releaseYear = 2020,
             resellPrice = 200
         ),
-
-        Sneakers(
+        Sneaker(
             brand = "Adidas",
             imageUrl = "https://i.postimg.cc/x8rPMBgs/adidas-yeezy-boost.jpg",
             modelName = "Yeezy Boost 350 V2",
             releaseYear = 2022,
             resellPrice = 350
         ),
-        Sneakers(
+        Sneaker(
             brand = "Jordan",
             imageUrl = "https://i.postimg.cc/5ycmLDnc/air-jordan-1-retro.jpg",
             modelName = "Air Jordan 1 Retro",
             releaseYear = 2021,
             resellPrice = 450
         ),
-        Sneakers(
+        Sneaker(
             brand = "New Balance",
             imageUrl = "https://i.postimg.cc/j2VhPBv9/new-balance-550.jpg",
             modelName = "550",
             releaseYear = 2023,
             resellPrice = 180
         ),
-    )
-    Sneakers(
-        brand = "Puma",
-        imageUrl = "https://i.postimg.cc/WzR7gxXP/puma-rsx.jpg",
-        modelName = "RS-X",
-        releaseYear = 2023,
-        resellPrice = 140
-    )
-
-    Sneakers(
-        brand = "Converse",
-        imageUrl = "https://i.postimg.cc/Y0cxYT8n/converse-chuck-70.jpg",
-        modelName = "Chuck 70",
-        releaseYear = 2013,
-        resellPrice = 100
-    )
-    Sneakers(
-        brand = "Reebok",
-        imageUrl = "https://i.postimg.cc/5ycmLDpr/reebok-club-c-85.jpg",
-        modelName = "Club C 85",
-        releaseYear = 2021,
-        resellPrice = 95
-    )
-    Sneakers(
-        brand = "Asics",
-        imageUrl = "https://i.postimg.cc/HnGtXF3q/asics-gel-kayano-14.jpg",
-        modelName = "Gel-Kayano 14",
-        releaseYear = 2022,
-        resellPrice = 215
+        Sneaker(
+            brand = "Puma",
+            imageUrl = "https://i.postimg.cc/WzR7gxXP/puma-rsx.jpg",
+            modelName = "RS-X",
+            releaseYear = 2023,
+            resellPrice = 140
+        ),
+        Sneaker(
+            brand = "Converse",
+            imageUrl = "https://i.postimg.cc/Y0cxYT8n/converse-chuck-70.jpg",
+            modelName = "Chuck 70",
+            releaseYear = 2013,
+            resellPrice = 100
+        ),
+        Sneaker(
+            brand = "Reebok",
+            imageUrl = "https://i.postimg.cc/5ycmLDpr/reebok-club-c-85.jpg",
+            modelName = "Club C 85",
+            releaseYear = 2021,
+            resellPrice = 95
+        ),
+        Sneaker(
+            brand = "Asics",
+            imageUrl = "https://i.postimg.cc/HnGtXF3q/asics-gel-kayano-14.jpg",
+            modelName = "Gel-Kayano 14",
+            releaseYear = 2022,
+            resellPrice = 215
+        )
     )
 
     val db = Firebase.firestore
@@ -171,5 +176,4 @@ private fun seeDataBase() {
                 Log.e("FIREBASE_TEST", "Błąd podczas dodawania: ", e)
             }
     }
-}
-
+}*/
